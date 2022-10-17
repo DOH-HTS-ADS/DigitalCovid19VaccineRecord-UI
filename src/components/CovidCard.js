98325c45343f23c7fa1e7c990d179286fa5776f7
James Allen <82835365+DOH-JRA2303@users.noreply.github.com>
2022-10-17 14:10:39 -0700

    Revert "Fix finalCheck function to check new Date of Birth fields"

--- src/components/CovidCard.js
+++ src/components/CovidCard.js
@@ -265,7 +265,7 @@ const CovidCard = () => {
       tempErrorObj.Pin = true;
       document.getElementById("partitioned").style.background = "repeating-linear-gradient(90deg, #b30000 0, #b30000 1ch, transparent 0, transparent 1.5ch) 0 100%/100% 2px no-repeat"
     }
-    if (!(monthOfBirth && dayOfBirth && yearOfBirth)) {
+    if (document.getElementById('dob') ? document.getElementById('dob').value.length < 1 : '') {
       tempErrorObj.Date = true;
     }
 
