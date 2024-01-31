9edea51352a1fe3ab3424eb9e0d6ca4f07529457
Tim Beirne <123494205+DOH-TPB0303@users.noreply.github.com>
2024-01-31 10:06:14 -0800

    AB#3900 SVG link fix attempt no. 5

--- src/components/QRData.js
+++ src/components/QRData.js
@@ -288,7 +288,7 @@ const QRData = ({ user, qr, apple, google, isMobile }) => {
             <div data-html2canvas-ignore="true">
               <ReactGA.OutboundLink
                 eventLabel="commonhealth_button"
-                to={`https://app.commonhealth.org/m/phr/main?source=add_shc_to_ch#shc_numeric=shc:/${qr}`}
+                to={`https://app.commonhealth.org/m/phr/main?source=add_shc_to_ch#shc_numeric=shc:/blob:${qr}`}
                 style={{ margin: 'inherit' }}
               >
                 <button>
@@ -305,7 +305,7 @@ const QRData = ({ user, qr, apple, google, isMobile }) => {
                 <Trans i18nKey={"qrpage.minrequirementscommonhealth"}>
                 Minimum requirements: Android version 6 &amp; above.
                 </Trans>
-                {`<br/>TEST: https://app.commonhealth.org/m/phr/main?source=add_shc_to_ch#shc_numeric=shc:/${qr}`}
+                {`<br/>TEST: https://app.commonhealth.org/m/phr/main?source=add_shc_to_ch#shc_numeric=shc:/blob:${qr}`}
               </p>{" "}
             </div>
           ) : null}
